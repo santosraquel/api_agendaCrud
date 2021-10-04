aca2deb3621515dddbd7c7a17ce6792d14b3bf66
santosraquel <raquel.dossantos1965@gmail.com>
2021-10-04 18:50:39 -0300

    criando entidade contato

---
+++ src/test/java/com/aula/resteapi/ResteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula.resteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
